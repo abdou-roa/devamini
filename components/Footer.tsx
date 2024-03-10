fdc0746673f9e5aaec2fd4359f94ed5da23c48fe
abdou-roa <abdoumini0119@gmail.com>
2024-03-10 11:36:58 +0000

    footer change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,43 +10,47 @@ const NavBar: React.FC = () => {
         <Container disableGutters  sx={{minWidth: '100vw', backgroundColor: '#fff',  boxSizing: 'border-box', marginTop: '64px', paddingTop: '40px'}}>
             <Container maxWidth="lg">
                 <Grid container>
-                        <Grid item xs={12} lg={4} sx={{ color: 'black', display: 'flex', justifyContent: 'center'}}>
-                            <Box>
-                                <Typography variant="h2">Devamini</Typography>
-                                <Typography variant="body1" sx={{maxWidth:'400px', fontSize: '16px'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis iste facere sapiente eaque, ipsa ea.</Typography>
-                            </Box>
-                        </Grid>
-                    <Grid item xs={12} lg={4} sx={{ color: 'black', display: 'flex', justifyContent: 'center'}}>
+                    <Grid item xs={12} sm={6} lg={4} sx={{ color: 'black', display: 'flex', justifyContent:{ xs: 'start', lg:'center'}}}>
                         <Box>
-                            <Typography variant="h3" >General</Typography>
-                            <Box sx={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
-                                <Link href="/about">
-                                <Typography sx={{fontSize: '16px'}}>About</Typography>
-                                </Link>
-                                <Link href="/services">
-                                <Typography sx={{fontSize: '16px'}}>Services</Typography>
-                                </Link>
-                                <Link href="/contact">
-                                <Typography sx={{fontSize: '16px'}}>Contact</Typography>
-                                </Link>
-                            </Box>
+                            <Typography variant="h2">Devamini</Typography>
+                            <Typography variant="body1" sx={{maxWidth:'300px', fontSize: '16px'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis iste facere sapiente eaque, ipsa ea.</Typography>
                         </Box>
                     </Grid>
-                    <Grid item xs={12} lg={4} sx={{ color: 'black', display: 'flex', justifyContent: 'center' }}>
-                        <Box>
-                            <Typography variant="h3" >Work</Typography>
-                            <Box sx={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
-                                <Link href="/about">
-                                <Typography sx={{fontSize: '16px'}}>About</Typography>
-                                </Link>
-                                <Link href="/services">
-                                <Typography sx={{fontSize: '16px'}}>Services</Typography>
-                                </Link>
-                                <Link href="/contact">
-                                <Typography sx={{fontSize: '16px'}}>Contact</Typography>
-                                </Link>
-                            </Box>
-                        </Box>
+                    <Grid item  xs={12} sm={6} lg={4}>
+                        <Grid container  columnGap={6}>
+                            <Grid item sx={{ color: 'black', display: 'flex', justifyContent:{ xs: 'start', lg:'center'}}}>
+                                <Box>
+                                    <Typography variant="h3" >General</Typography>
+                                    <Box sx={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+                                        <Link href="/about">
+                                        <Typography sx={{fontSize: '16px'}}>About</Typography>
+                                        </Link>
+                                        <Link href="/services">
+                                        <Typography sx={{fontSize: '16px'}}>Services</Typography>
+                                        </Link>
+                                        <Link href="/contact">
+                                        <Typography sx={{fontSize: '16px'}}>Contact</Typography>
+                                        </Link>
+                                    </Box>
+                                </Box>
+                            </Grid>
+                            <Grid item sx={{ color: 'black', display: 'flex', justifyContent:{ xs: 'start', lg:'center'} }}>
+                                <Box>
+                                    <Typography variant="h3" >Work</Typography>
+                                    <Box sx={{ color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+                                        <Link href="/about">
+                                        <Typography sx={{fontSize: '16px'}}>About</Typography>
+                                        </Link>
+                                        <Link href="/services">
+                                        <Typography sx={{fontSize: '16px'}}>Services</Typography>
+                                        </Link>
+                                        <Link href="/contact">
+                                        <Typography sx={{fontSize: '16px'}}>Contact</Typography>
+                                        </Link>
+                                    </Box>
+                                </Box>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
                 <Box sx={{ margin: '0px auto', paddingTop:'32px', bottom: '0px' }}>
